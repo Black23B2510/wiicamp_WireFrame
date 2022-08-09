6678b9077ee8d472311c6d8c202c3e52b41e782c
Black23B2510 <den.ho23@student.passerellesnumeriques.org>
2022-08-09 13:40:40 +0700

    update responsive feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,23 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
       // dynamicBullets: true
     },
-  
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
     // And if we need scrollbar
     
   });
+  const mobileMenu = document.getElementById("mobile-menu");
+  const menu = document.getElementById("nav");
+  mobileMenu.onclick = function() {
+    menu.classList.toggle("active")
+  }
+  
